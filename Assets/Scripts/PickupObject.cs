94c67c271b24084ed601f5f63c8997d5f481337e
eeemeli <58853404+eeemeli@users.noreply.github.com>
2020-01-08 01:11:57 +0200

    kolikoiden keräys, pisteet konsoliin ja jotain muuta pientä kikkailua

---
+++ Assets/Scripts/PickupObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickupObject : MonoBehaviour
+{
+    public static int theScore;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            theScore += 1;
+            print("KERÄTYT KOLIKOT " + theScore + "/6");
+            Destroy(gameObject);
+        }
+        if(theScore == 6)
+        {
+            print("Keräsit kaikki kolikot!");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ScoringSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoringSystem : MonoBehaviour
+{
+
+    public GameObject scoreText;
+    public int theScore;
+
+    void OnTriggerEnter(Collider other)
+    {
+        theScore += 50;
+        scoreText.GetComponent<Text>().text = "SCORE: " + theScore;
+        Destroy(gameObject);
+    }
+
+
+// Start is called before the first frame update
+void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
